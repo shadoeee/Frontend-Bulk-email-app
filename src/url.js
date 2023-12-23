92ddf67f80c37b147d4d6bb92306fc0fdb66e150
shadoeee <balaganesh541@gmail.com>
2023-12-23 20:04:09 +0530

    hi

--- src/url.js
+++ src/url.js
@@ -1,4 +1,4 @@
-//  const API = "http://localhost:5000"
-const API = "https://backend-bulk-mail-app.vercel.app"
+ const API = "http://localhost:5000"
+// const API = "https://backend-bulk-mail-app.vercel.app"
 
  export default API
